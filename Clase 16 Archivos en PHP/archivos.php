b406b7930e04480078c59cd24434b86c40890455
Osvaldo Cantone <ocantone@gmail.com>
2026-05-28 12:16:20 -0300

    Clase 16 manejando archivos con PHP

---
+++ Clase 16 Archivos en PHP/archivos.php
@@ -0,0 +1,28 @@
+<?php
+
+$contenido = $_POST['contarchivo'];
+$nombre = $_POST['nomarchivo'];
+
+$archivo = fopen($nombre,"w");
+fwrite($archivo, $contenido);
+fclose($archivo);
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>  
+
+</head>
+<body>
+Arcchivo <? echo $nombre ?> creado con éxito
+
+
+<p><a href="index.php">Volver</a></p>
+
+
+</body>
+</html>
