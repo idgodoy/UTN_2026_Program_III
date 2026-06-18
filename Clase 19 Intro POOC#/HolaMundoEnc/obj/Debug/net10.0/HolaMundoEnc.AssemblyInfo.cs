e557d86b3f32963daf7b3cf3f443086badb941c4
Osvaldo Cantone <ocantone@gmail.com>
2026-06-18 09:27:57 -0300

    Agrego Excepciones

--- Clase 19 Intro POOC#/HolaMundoEnc/obj/Debug/net10.0/HolaMundoEnc.AssemblyInfo.cs
+++ Clase 19 Intro POOC#/HolaMundoEnc/obj/Debug/net10.0/HolaMundoEnc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HolaMundoEnc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7603488299211223978de0b0bb76e9386ccadf07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dabe9173b41057434a3f4d866738f227ae5dd441")]
 [assembly: System.Reflection.AssemblyProductAttribute("HolaMundoEnc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HolaMundoEnc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Clase21_Persistencia_Archivos/QueueArchivoTxt/obj/Debug/net10.0/QueueArchivoTxt.AssemblyInfo.cs
+++ Clase21_Persistencia_Archivos/QueueArchivoTxt/obj/Debug/net10.0/QueueArchivoTxt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueueArchivoTxt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1b787d102f3ae961fb91901124b707d99200f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dabe9173b41057434a3f4d866738f227ae5dd441")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueueArchivoTxt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QueueArchivoTxt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
